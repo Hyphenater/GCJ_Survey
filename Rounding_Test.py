cec943c75dc932bf5d86b4c8b86b5ef58531d448
Hyphenater <55498719+Hyphenater@users.noreply.github.com>
2019-09-18 16:11:32 +0100

    Delete Rounding_Test.py
    Mistakenly uploaded at first

--- Rounding_Test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-nums = [30, 40, 21, 8, 10]
-percentages = [num / sum(nums) * 100 for num in nums]
-print(percentages)
-
-percentage_sum = sum(percentages)
-print(percentage_sum)
